740d98dd7eae0e1bade5ffa0ebf549deceeaf9c5
pyexpert <pyexpert1117@gmail.com>
2019-07-19 00:30:00 +0800

    searchpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './components/mainpage/mainpage.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:MainPageComponent, data: {title: 'Home'}},
+  {path: 'login', component:LoginComponent},
+  // default redirect
+  {path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './components/login/login.component'
+import { MainComponent } from './components/common/main.component'
+import { DetailItemComponent } from './components/mainpage/detailitem.component'
+import { MainPageComponent } from './components/mainpage/mainpage.component';
+import { SearchBarComponent } from './components/mainpage/childcomponent/searchbar.component';
+import { SearchResultComponent } from './components/mainpage/childcomponent/searchresult.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    DetailItemComponent,
+    MainPageComponent,
+    SearchBarComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
